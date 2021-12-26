c74d0c8e39c2cf59356923176a99f32427b68660
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-12-25 19:27:28 -0600

    Original program handling just one async method

--- AsyncExample/Program.cs
+++ AsyncExample/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncExample
 {
     class Program
     {
+        private static async Task<string> DelayedMethodThatSucceds()
+        {
+            await Task.Delay(4000);
+            Console.WriteLine("Working delayedMethodThatSucceds");
+            return "Success delayedMethodThatSucceds";
+        }
+
+        private static async Task<string> DelayedMethodThatFails()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Working delayedMethodThatFails");
+            throw new Exception("This is an exception on delayedMethodThatFails");
+            //return "Success delayedMethodThatFails";
+        }
+
+
+        private static async Task MethodHandlingOneTask()
+        {
+            Console.WriteLine("Starting MethodHandlingOneTask");
+
+            Task<string> DelayedMethodThatFailsTask = DelayedMethodThatFails();
+
+            try
+            {
+                string data = await DelayedMethodThatFailsTask;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Ending MethodHandlingOneTask");
+        }
+
+        private static async Task MainAsync()
+        {
+            await MethodHandlingOneTask();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
         }
     }
 }
